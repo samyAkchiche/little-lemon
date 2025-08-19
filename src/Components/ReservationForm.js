7b7d4be0769adbc17c31bde3a2b724658ae7633f
Nagi.akc <samyakchiche04@gmail.com>
2025-08-19 10:36:25 +0100

    Added a step2 form which consists of personal data like name, email, phone number...

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -7,11 +7,18 @@ export default function ReservationForm({
     submitForm,
 }) {
     const [dateError, setDateError] = useState("");
+    const [step, setStep] = useState(1);
     const [form, setForm] = useState({
         date: "",
         time: "",
         guests: 1,
         occasion: "Birthday",
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        specialRequest: "",
+        acceptedPolicy: false,
     });
 
     const validateDate = (selectedDate) => {
@@ -30,7 +37,7 @@ export default function ReservationForm({
     const isGuestsInvalid =
         form.guests !== 1 && (form.guests < 1 || form.guests > 10);
 
-    const getIsFormValid = () => {
+    const getIsFormValidStepOne = () => {
         return (
             form.date &&
             form.time &&
@@ -40,6 +47,23 @@ export default function ReservationForm({
         );
     };
 
+    const getIsFormValidStepTwo = () => {
+        return (
+            form.firstName &&
+            form.lastName &&
+            form.email &&
+            form.phoneNumber &&
+            form.acceptedPolicy
+        );
+    };
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        if (getIsFormValidStepOne()) {
+            setStep(2);
+        }
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         submitForm(form);
@@ -47,80 +71,179 @@ export default function ReservationForm({
     };
 
     return (
-        <form className={styles.form} onSubmit={handleSubmit}>
-            <label htmlFor="res-date">Choose Date</label>
-            <input
-                type="date"
-                id="res-date"
-                value={form.date}
-                required
-                onChange={(e) => {
-                    const newDate = e.target.value;
-                    setForm({ ...form, date: newDate, time: "" });
-                    validateDate(newDate);
-                    dispatch({ type: "update", payload: newDate });
-                }}
-            />
-            {dateError && <p>{dateError}</p>}
-            <label htmlFor="res-time">Choose Time</label>
-            <select
-                id="res-time"
-                value={form.time}
-                onChange={(e) => setForm({ ...form, time: e.target.value })}
-                required
-            >
-                <option value="" disabled>
-                    Select a Time
-                </option>
-                {availableTimes && availableTimes.length > 0 ? (
-                    availableTimes.map((time) => {
-                        return (
-                            <option key={time} value={time}>
-                                {time}
-                            </option>
-                        );
-                    })
-                ) : (
-                    <option disabled>No available times</option>
-                )}
-            </select>
-            <label htmlFor="guests">Number of Guests</label>
-            <input
-                type="number"
-                placeholder="1"
-                min={1}
-                max={10}
-                id="guests"
-                value={form.guests}
-                onChange={(e) =>
-                    setForm({ ...form, guests: Number(e.target.value) })
-                }
-                aria-invalid={isGuestsInvalid}
-                aria-describedby="guests-error"
-                required
-            ></input>
-            {isGuestsInvalid && (
-                <p id="guests-error" className={styles.errorText} role="alert">
-                    * Please enter a number between 1 and 10.
-                </p>
+        <>
+            {step === 1 && (
+                <form className={styles.form} onSubmit={handleNext}>
+                    <label htmlFor="res-date">Choose Date</label>
+                    <input
+                        type="date"
+                        id="res-date"
+                        value={form.date}
+                        required
+                        onChange={(e) => {
+                            const newDate = e.target.value;
+                            setForm({ ...form, date: newDate, time: "" });
+                            validateDate(newDate);
+                            dispatch({ type: "update", payload: newDate });
+                        }}
+                    />
+                    {dateError && <p role="alert">{dateError}</p>}
+                    <label htmlFor="res-time">Choose Time</label>
+                    <select
+                        id="res-time"
+                        value={form.time}
+                        onChange={(e) =>
+                            setForm({ ...form, time: e.target.value })
+                        }
+                        required
+                    >
+                        <option value="" disabled>
+                            Select a Time
+                        </option>
+                        {availableTimes && availableTimes.length > 0 ? (
+                            availableTimes.map((time) => {
+                                return (
+                                    <option key={time} value={time}>
+                                        {time}
+                                    </option>
+                                );
+                            })
+                        ) : (
+                            <option disabled>No available times</option>
+                        )}
+                    </select>
+                    <label htmlFor="guests">Number of Guests</label>
+                    <input
+                        type="number"
+                        placeholder="1"
+                        min={1}
+                        max={10}
+                        id="guests"
+                        value={form.guests}
+                        onChange={(e) =>
+                            setForm({ ...form, guests: Number(e.target.value) })
+                        }
+                        aria-invalid={isGuestsInvalid}
+                        aria-describedby="guests-error"
+                        required
+                    />
+                    {isGuestsInvalid && (
+                        <p
+                            id="guests-error"
+                            className={styles.errorText}
+                            role="alert"
+                        >
+                            * Please enter a number between 1 and 10.
+                        </p>
+                    )}
+                    <label htmlFor="occasion">Occasion</label>
+                    <select
+                        id="occasion"
+                        value={form.occasion}
+                        onChange={(e) =>
+                            setForm({ ...form, occasion: e.target.value })
+                        }
+                        required
+                    >
+                        <option value="Birthday">Birthday</option>
+                        <option value="Anniversary">Anniversary</option>
+                    </select>
+                    <input
+                        type="submit"
+                        value="Make Your Reservation"
+                        className={styles.submitButton}
+                        disabled={!getIsFormValidStepOne()}
+                        aria-label="On Click"
+                    />
+                </form>
             )}
-            <label htmlFor="occasion">Occasion</label>
-            <select
-                id="occasion"
-                value={form.occasion}
-                onChange={(e) => setForm({ ...form, occasion: e.target.value })}
-                required
-            >
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-            </select>
-            <input
-                type="submit"
-                value="Make Your Reservation"
-                className={styles.submitButton}
-                disabled={!getIsFormValid()}
-                aria-label="On Click"
-            />
-        </form>
+            {step === 2 && (
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        value={form.firstName}
+                        onChange={(e) => {
+                            setForm({ ...form, firstName: e.target.value });
+                        }}
+                        required
+                    ></input>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        value={form.lastName}
+                        onChange={(e) => {
+                            setForm({ ...form, lastName: e.target.value });
+                        }}
+                        required
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        value={form.email}
+                        onChange={(e) => {
+                            setForm({ ...form, email: e.target.value });
+                        }}
+                        required
+                    />
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input
+                        type="tel"
+                        id="phoneNumber"
+                        value={form.phoneNumber}
+                        onChange={(e) => {
+                            setForm({ ...form, phoneNumber: e.target.value });
+                        }}
+                        required
+                    />
+                    <label htmlFor="specialRequest">Special Request</label>
+                    <textarea
+                        id="specialRequest"
+                        type="text"
+                        value={form.specialRequest}
+                        onChange={(e) => {
+                            setForm({
+                                ...form,
+                                specialRequest: e.target.value,
+                            });
+                        }}
+                    ></textarea>
+                    <div className={styles.checkboxGroup}>
+                        <input
+                            type="checkbox"
+                            id="acceptedPolicy"
+                            checked={form.acceptedPolicy}
+                            onChange={(e) => {
+                                setForm({
+                                    ...form,
+                                    acceptedPolicy: e.target.checked,
+                                });
+                            }}
+                            required
+                        />
+                        <label htmlFor="acceptedPolicy">
+                            I agree to the
+                            <a
+                                href="#"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Privacy Policy
+                            </a>
+                        </label>
+                    </div>
+                    <input
+                        type="submit"
+                        value="Confirm Your Reservation"
+                        aria-label="on Click"
+                        className={styles.submitButton}
+                        disabled={!getIsFormValidStepTwo()}
+                    />
+                </form>
+            )}
+        </>
     );
 }
